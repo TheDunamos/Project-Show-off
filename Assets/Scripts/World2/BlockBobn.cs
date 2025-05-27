3d35fd0e227a13f4a59cde7c270a997c66e04d4b
TheDunamos <viggo.lensink@gmail.com>
2025-05-27 14:20:33 +0200

    World1 complete, working on pt.2 interactions

--- Assets/Scripts/World2/BlockBobn.cs
+++ Assets/Scripts/World2/BlockBobn.cs
@@ -4,15 +4,33 @@ public class BlockBobn : MonoBehaviour
 {
     public float speedUpDown = 3;
     public float distanceUpDown = 1;
+    private GameObject VarManager;
+
+
+
     void Start()
     {
-
+        GetComponent<BoxCollider>().enabled = false;
+        VarManager = GameObject.FindGameObjectWithTag("VarManager");
     }
 
     void Update()
+    {
+        GlobalVars Vars = VarManager.GetComponent(typeof(GlobalVars)) as GlobalVars;
+
+/*        if ()
+        {
+            Move();
+        }*/
+
+    }
+
+
+    private void Move()
     {
         Vector3 mov = new Vector3(transform.position.x, Mathf.Sin(speedUpDown * Time.time) * distanceUpDown + transform.parent.position.y, transform.position.z);
         transform.position = mov;
         transform.rotation = transform.parent.rotation;
     }
+
 }
